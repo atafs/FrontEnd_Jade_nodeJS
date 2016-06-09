13968e1d16b2648c87a3c302ebe4a3e1b699e8dd
atafs <atafs@iscte-iul.pt>
2016-06-09 11:57:19 +0100

    more proj in jade

--- _bat/_1start.bat
+++ _bat/_1start.bat
@@ -3,9 +3,10 @@ npm install jade -g
 
 REM 2_ open and save a file with the extension .jade
 
-REM 3_ render file (sort of a compilation)
+REM 3_ render file (sort of a compilation) HTML
 jade _1begin.jade
 
+REM for generating Javascript file
+jade --client --no-debug _2sintaxe.jade
+
 REM 4_ run html page in web
-
-
