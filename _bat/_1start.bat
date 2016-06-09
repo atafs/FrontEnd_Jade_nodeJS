1203482064abc7e81329918e471ad3df7d133181
atafs <atafs@iscte-iul.pt>
2016-06-09 10:30:01 +0100

    proj001 start

---
+++ _bat/_1start.bat
@@ -0,0 +1,11 @@
+REM 1_ install jade
+npm install jade -g
+
+REM 2_ open and save a file with the extension .jade
+
+REM 3_ render file (sort of a compilation)
+jade _1begin.jade
+
+REM 4_ run html page in web
+
+
